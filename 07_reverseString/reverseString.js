be51888efc553cfc5e490f94b9d2813df7729ce1
hebeishy <30864033+hebeishyy@users.noreply.github.com>
2025-05-08 10:59:09 +0300

    finish 07 ex

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,4 +1,13 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let arr = string.split("");
+    let rev = [];
+    let newString = "";
+    for (let i = 0; i <= string.length; i++)
+    {
+        rev[i] = arr[string.length - i];
+    }
+    newString = rev.join("");
+    return newString;
 
 };
 
