660f4f7b486efae6856063ba7534d6e8f1eb6424
hebeishy <30864033+hebeishyy@users.noreply.github.com>
2025-05-10 05:20:18 +0300

    finish 08 ex w 2 methods

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,32 @@
-const removeFromArray = function() {
+function removeFromArray(array, ...value) {
+    let result = [];
+    for (let i = 0; i < array.length; i++)
+    {
+        if (!value.includes(array[i]))
+        {
+           result.push(array[i]);
+        }
+    }
+    return result
 };
+
+/*function remove2(array, ...value){
+    let result = [];
+    for (let i = 0; i < array.length; i++)
+    {
+        if (!value.includes(array[i]))
+        {
+           result.push(array[i]);
+        }
+    }
+    return result
+
+    let result = array.filter(x => !value.includes(x))
+    return result;
+} */
+
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
