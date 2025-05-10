ab8ab32716fa58cac38561d2a00c32b63c8f5d5a
hebeishy <30864033+hebeishyy@users.noreply.github.com>
2025-05-10 18:49:14 +0300

    finish all ex in arrayNLoops module

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,36 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    let result = 0;
+    if (a < 0 || b < 0)
+    {
+        return "ERROR";
+    }
+    if (isNaN(a) || isNaN(b))
+    {
+        return "ERROR";
+    }
+    if (a !== Math.floor(a) || b !== Math.floor(b))
+    {
+        return "ERROR";
+    }
+    if( a < b)
+    {
+        for (let i = a; i <= b; i++)
+            {
+                result = result + i;
+            }
+            return result;
+    } else if ( b < a)
+    {
+        for (let i = b; i <= a; i++)
+            {
+                result = result + i;
+            }
+            return result;
+    }
+    
 };
+
+console.log(sumAll(1,4));
 
 // Do not edit below this line
 module.exports = sumAll;
--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,14 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahr) {
+  let celsius = (fahr - 32) * (5/9);
+  celsius = Number(celsius.toFixed(1));
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  let fahr = (cels * 9/5) + 32;
+
+  fahr = Number(fahr.toFixed(1));
+  return fahr;
 };
 
 // Do not edit below this line
