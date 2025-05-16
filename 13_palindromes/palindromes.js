b58cfe1b23f9ce1ed71a48ab79341b7ed39fa7eb
hebeishy <30864033+hebeishyy@users.noreply.github.com>
2025-05-16 08:33:37 +0300

    finish 13 ex

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,25 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let lowerString = string.toLowerCase();
+    array = lowerString.split("");
+    for (let i = array.length; i > 0; i--)
+        {
+        if (array[i] == '.' || array[i] == ' ' || array[i] == ',' || array[i] == '!')
+        {
+            array.splice(i, 1);
+        }
+    }
+    let original = array.join("");
+    let reversed = array.reverse().join("");
 
+    console.log(original);
+    console.log(reversed);
+    if (original === reversed)
+    {
+        return true;
+    } else {
+        return false;
+    }
+    
 };
 
 // Do not edit below this line
